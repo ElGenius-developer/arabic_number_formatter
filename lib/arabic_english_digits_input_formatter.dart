49528246ee3cb6a665d33223706c255056cfb16f
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2023-09-17 11:42:52 +0300

    Update arabic_english_digits_input_formatter.dart

--- lib/arabic_english_digits_input_formatter.dart
+++ lib/arabic_english_digits_input_formatter.dart
@@ -6,8 +6,11 @@ class ArabicEnglishOnlyDigitsInputFormatter extends TextInputFormatter {
       TextEditingValue oldValue, TextEditingValue newValue) {
     String filteredText = newValue.text;
 
-    // Filter out non-Arabic and non-English digits
-    filteredText = filteredText.replaceAll(RegExp(r'[^\u0660-\u06690-9]'), '');
+    if (filteredText.isNotEmpty) {
+      // Filter out non-Arabic and non-English digits
+      filteredText =
+          filteredText.replaceAll(RegExp(r'[^\u0660-\u06690-9]'), '');
+    }
 
     return TextEditingValue(
       text: filteredText,
