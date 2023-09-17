b31f3f24f378edbdb2bf9f91636c64a1eb7f7ac9
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2023-09-17 11:47:09 +0300

    Update zero_remove_formatter.dart

--- lib/zero_remove_formatter.dart
+++ lib/zero_remove_formatter.dart
@@ -1,21 +1,13 @@
 part of 'arabic_number_formatter.dart';
 
 class RemoveLeadingZeroFormatter extends TextInputFormatter {
-
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
     String newText = newValue.text;
 
-    if (newText.isNotEmpty) {
-      // Check if the first character is a zero digit (either Arabic or English)
-      bool isFirstCharZero = newText.startsWith('0') || newText.startsWith('٠');
-
-      // Remove the leading zero digit if present
-      if (isFirstCharZero) {
-        newText = newText.substring(1);
-      }
-    }
+    // Remove leading zero digits (either Arabic or English)
+    newText = newText.replaceAll(RegExp(r'^[0٠]+'), '');
 
     return TextEditingValue(
       text: newText,
