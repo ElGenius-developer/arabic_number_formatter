0033492648a13e3aadbae3fa7f6acdca4baaa33a
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2023-09-24 17:55:22 +0300

    Update zero_remove_formatter.dart

--- lib/zero_remove_formatter.dart
+++ lib/zero_remove_formatter.dart
@@ -1,14 +1,14 @@
 part of 'arabic_number_formatter.dart';
 
 class RemoveLeadingZeroFormatter extends TextInputFormatter {
-  final  void Function()? onZeroEnter;
-  RemoveLeadingZeroFormatter({this.onZeroEnter});
+  final  void Function()? onZeroEntered;
+  RemoveLeadingZeroFormatter({this.onZeroEntered});
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
     if (newValue.text.isNotEmpty && (newValue.text.startsWith('0')|| newValue.text.startsWith('٠'))) {
-      if(onZeroEnter!=null){
-        onZeroEnter!();
+      if(onZeroEntered!=null){
+        onZeroEntered!();
         }
       // Prevent the input if it starts with '0'
       return oldValue;
