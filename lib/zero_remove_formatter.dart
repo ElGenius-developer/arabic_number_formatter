a05a645cc6bf4b269976934548251772f0f49a9b
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2023-09-24 17:54:07 +0300

    Update zero_remove_formatter.dart

--- lib/zero_remove_formatter.dart
+++ lib/zero_remove_formatter.dart
@@ -1,26 +1,15 @@
 part of 'arabic_number_formatter.dart';
 
 class RemoveLeadingZeroFormatter extends TextInputFormatter {
-  final BuildContext context ;
-  RemoveLeadingZeroFormatter(this.context);
+  final  void Function()? onZeroEnter;
+  RemoveLeadingZeroFormatter({this.onZeroEnter});
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
     if (newValue.text.isNotEmpty && (newValue.text.startsWith('0')|| newValue.text.startsWith('٠'))) {
-       final snackBar = SnackBar(
-              content: Text(
-               "Enter phone number without 0 ",
-              ),
-
-              // closeIconColor: textColor,
-              backgroundColor: const Color(0x7b0e16ab),
-              duration: const Duration(seconds: 2),
-              showCloseIcon: true,
-              dismissDirection: DismissDirection.down,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)),
-            );
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      if(onZeroEnter!=null){
+        onZeroEnter!();
+        }
       // Prevent the input if it starts with '0'
       return oldValue;
     }
