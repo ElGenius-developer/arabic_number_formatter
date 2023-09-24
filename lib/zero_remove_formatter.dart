ec8af4a678eb14ac7b72cc7e6013a6085e8376cc
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2023-09-24 17:34:30 +0300

    Update zero_remove_formatter.dart

--- lib/zero_remove_formatter.dart
+++ lib/zero_remove_formatter.dart
@@ -5,6 +5,23 @@ class RemoveLeadingZeroFormatter extends TextInputFormatter {
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
     if (newValue.text.isNotEmpty && (newValue.text.startsWith('0')|| newValue.text.startsWith('٠'))) {
+       final snackBar = SnackBar(
+              content: CustomText(
+                size: 17,
+                text: "Enter phone number without 0 ",
+                textOverflow: TextOverflow.visible,
+                color: Colors.white,
+              ),
+
+              // closeIconColor: textColor,
+              backgroundColor: const Color(0x7b0e16ab),
+              duration: const Duration(seconds: 2),
+              showCloseIcon: true,
+              dismissDirection: DismissDirection.down,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+            );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
       // Prevent the input if it starts with '0'
       return oldValue;
     }
